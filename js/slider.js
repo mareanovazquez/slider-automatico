ff2d6b6f41399ccd1d9bfa63bc0eda0ef4ea9b59
Mariano Vazquez <78172034+mareanovazquez@users.noreply.github.com>
2024-01-14 21:26:26 -0300

    ajustes en la transición

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,19 @@
-let slideIndex = 1; // Inicializar en 1 en lugar de 0
+let slideIndex = 1; // Inicializado en 0 para que la primera transición sea más suave
 
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slideIndex++;
+  
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
+  
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Cambiar la imagen cada 3 segundos
 }
